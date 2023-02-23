b09bb1e61b579112d055cacf66c7c4904a1ad1e5
Ashish Jain <ashish.ashish.jain591@gmail.com>
2023-02-23 12:17:14 +0530

    server changes

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ import express from "express";
 import authRoutes from "./authNew.js";
 import userRoutes from "./user.js";
 import rideRoutes from "./ride.js";
-import userRidesRoutes from "./userRide.js";
+// import userRidesRoutes from "./userRide.js";
 import rideConnectionRoutes from "./rideConnection.js";
 import connectionRoutes from "./connection.js";
 import messageRoutes from "./message.js";
@@ -12,13 +12,11 @@ import { requireSignIn } from "../middleware/auth.js";
 const rootRouter = express.Router();
 
 rootRouter.use("/auth", authRoutes);
-rootRouter.use("/auth", authRoutes);
-rootRouter.use("/user", userRoutes);
+rootRouter.use("/user", requireSignIn, userRoutes);
 rootRouter.use("/ride", requireSignIn, rideRoutes);
-rootRouter.use("/userRides", userRidesRoutes);
 rootRouter.use("/feedback", feedbackRoutes);
-rootRouter.use("/connection", connectionRoutes);
-rootRouter.use("/rideConnection", rideConnectionRoutes);
-rootRouter.use("/message", messageRoutes);
+rootRouter.use("/connection", requireSignIn, connectionRoutes);
+rootRouter.use("/rideConnection", requireSignIn, rideConnectionRoutes);
+rootRouter.use("/message", requireSignIn, messageRoutes);
 
 export default rootRouter;
--- socket.js
+++ socket.js
@@ -1,3 +1,5 @@
+import { socketConstants } from "./helpers/socketConstants.js";
+
 const addUser = (userId, socketId) => {
   !users.some((user) => user.userId === userId) &&
     users.push({
@@ -17,24 +19,44 @@ const getUser = (userId) => {
 let users = [];
 
 export const setupSocketIO = (io) => {
-  io.on("connection", (socket) => {
+  io.on(socketConstants.CONNECTION, (socket) => {
     //Add new user
-    socket.on("add-user", (userId) => {
+    socket.on(socketConstants.ADD_USER, (userId) => {
       addUser(userId, socket.id);
-      io.emit("get-users", users);
+      io.emit(socketConstants.GET_USERS, users);
     });
 
-    socket.on("send-message", (messageData, receiverId) => {
+    //Handle messages
+    socket.on(socketConstants.SEND_MESSAGE, (messageData, receiverId) => {
       const receiver = getUser(receiverId);
       if (receiver?.socketId) {
-        io.to(receiver.socketId).emit("receive-message", messageData);
+        io.to(receiver.socketId).emit(
+          socketConstants.RECEIVE_MESSAGE,
+          messageData
+        );
       }
     });
 
+    //Handle connections
+    socket.on(socketConstants.ADD_CONNECTION, (connectionData, receiverId) => {
+      const receiver = getUser(receiverId);
+      if (receiver?.socketId) {
+        io.to(receiver.socketId).emit(
+          socketConstants.CONNECTION_ADDED,
+          connectionData
+        );
+      }
+    });
+
+    //Handle ride
+    socket.on(socketConstants.CREATE_RIDE, (rideData) => {
+      socket.broadcast.emit(socketConstants.GET_RIDE, rideData);
+    });
+
     //Disconnecting the user
-    socket.on("disconnect", () => {
+    socket.on(socketConstants.DISCONNECT, () => {
       removeUser(socket.id);
-      io.emit("get-users", users);
+      io.emit(socketConstants.GET_USERS, users);
     });
   });
 };
